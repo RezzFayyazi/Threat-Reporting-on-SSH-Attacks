9092a78bcf600b70638da8c88396730f9a9aeca4
RezzFayyazi <Rezzaa.fa@gmail.com>
2023-12-15 13:36:55 -0500

    finalized code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import openai
 import time
 import pandas as pd
-
-
-
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 def get_completion(prompt, model="gpt-4-1106-preview"):
-    openai.api_key = "YOUR_API_KEY"
+    openai.api_key = config.get('API', 'OpenAI_Key')
     messages = [{"role": "system", 
                  "content":"You are a cybersecurity expert with an in-depth knowledge about SSH attacks."},
                 {"role": "user", "content": prompt}]
@@ -18,18 +18,34 @@ def get_completion(prompt, model="gpt-4-1106-preview"):
     )
     return response.choices[0].message["content"]
 
+def extract_commands(file_path):
+    commands = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        if lines and "CMD: " in lines[-1]:
+            command = lines[-1].split("CMD: ")[1].strip()
+            commands.append(command)
+    return ' '.join(commands)
 
-def prediction(csv_path):
+def read_num(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return int(file.read().strip())
+    except FileNotFoundError:
+        return 1  # Return 1 if the file doesn't exist
+
+def write_num(file_path, num):
+    with open(file_path, 'w') as file:
+        file.write(str(num))
+
+def prediction(file_path):
     predictions = []
-    #df = pd.read_csv(csv_path)
-    #timestamp_command_list = []
-    #for timestamp, command in zip(df['timestamp'], df['commands']):
-    #    timestamp_command_list.append((timestamp, command))
-
+    extracted_commands = extract_commands(file_path)
+    print(extracted_commands)
+    
     prompt = f"""For the malicious SSH command below, what is the intent of adversaries on my system?
 
-    SSH Command: mv /usr/bin/curl /usr/bin/cd1
-
+    SSH Command: {extracted_commands}
 
     Please break down the command into its components and explain the intent of the adversary. Finally, provide the MITRE ATT&CK tactic(s) that the adversary will achieve with this technique.
     """
@@ -43,11 +59,18 @@ def prediction(csv_path):
             break
         except (openai.error.RateLimitError, openai.error.APIError, openai.error.Timeout,
                 openai.error.OpenAIError, openai.error.ServiceUnavailableError):
-            time.sleep(10)
+            time.sleep(3)
     return predictions
 
 if __name__ == "__main__":
-    predictions = prediction(csv_path='./Data/commands.csv')
+    num_file_path = './Data/last_num.txt'
+    num = read_num(num_file_path)
+    file_path = f'./Data/cowrie-log.txt'
+    
+    predictions = prediction(file_path)
     df = pd.DataFrame(predictions)
     print(df)
-    #df.to_csv('./preds_gpt-4-turbo_prompt_only.csv', index=False)
+    df.to_csv(f'./Results/threat_report_{num}.csv', index=False)
+    
+    num += 1
+    write_num(num_file_path, num)
